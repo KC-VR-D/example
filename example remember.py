cccba7eb8c2754e48fcb62e11fb6d6b0f213c677
casey <casey@gmail.com>
2022-09-06 11:26:37 +0100

    starter read me

---
+++ example remember.py
@@ -0,0 +1,10 @@
+print()
+
+name=input(f"what is your name?")
+print(f"Hello{name}")
+age=int(input(f"how old are you?"))
+print(F"You will be {age+1} next year, right?")
+print(f"yh")
+
+
+print()
---
+++ python code 1.py
@@ -0,0 +1,14 @@
+
+print() #generates a blank line
+name=input("what is your name?")
+age=int(input(f"How old are you,{name}"))
+
+print(f"Hello {name}")
+print(f"You are {age} right?")
+
+print(f"I used to know someone else called{name}.")
+print(f"There is a wallet here is, it {name}'s.")
+print(f"you will be {age+1} next year, right?")
+print(f"yh")
+
+print()
